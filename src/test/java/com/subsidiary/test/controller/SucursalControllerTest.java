76367992107059bfa8dedb25739a5d430c75a0d5
jvaldez <valdez.jose.francisco@gmail.com>
2021-01-25 15:07:20 -0300

    v.1

--- src/test/java/com/subsidiary/test/controller/SucursalControllerTest.java
+++ src/test/java/com/subsidiary/test/controller/SucursalControllerTest.java
@@ -2,11 +2,22 @@ package com.subsidiary.test.controller;
 
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.subsidiary.test.model.Sucursal;
+import com.subsidiary.test.model.SucursalKpi;
+import com.subsidiary.test.repository.SucursalRepository;
 
 
 
